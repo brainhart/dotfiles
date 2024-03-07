df20a8fcc2088038b03128ff4f636ac6d66b4551
Brian Hart <brian@tecton.ai>
2024-03-06 21:58:29 -0800

    Update dot_config/nvim/lua/plugins/disabled.lua

--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,8 @@
 return {
-  -- disable trouble
-  { "echasnovski/mini.pairs", enabled = false },
+	-- disable trouble
+	{ "echasnovski/mini.pairs", enabled = false },
+	{ "JoosepAlviste/nvim-ts-context-commentstring", enabled = false },
+	{ "echasnovski/mini.surround", enabled = false },
+	{ "echasnovski/mini.ai", enabled = false },
+	{ "echasnovski/mini.bufremove", enabled = false },
 }
