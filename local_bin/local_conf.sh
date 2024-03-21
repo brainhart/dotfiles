dcdd89a98a5dac84a7d92e94bdc7f2b6eef17a49
Brian Hart <brian@tecton.ai>
2024-03-21 09:49:05 -0700

    Update local_bin/local_conf.sh

--- local_bin/local_conf.sh
+++ local_bin/local_conf.sh
@@ -19,7 +19,7 @@ alias start-mds='(cdt && bazel run //java/com/tecton/metadataservice:metadata_se
 alias start-grpc-gateway='(cdt && bazel run //go/cmd/grpc_gateway:grpc_gateway -- -metadata_service_url=localhost:50051 -allow_CORS=true)'
 alias repl="(cdt && bazel run //sdk:repl)"
 alias devton='bazel run //sdk:declarative_cli --'
-alias pin_deps='bazel run //python:pin_deps --//python/toolchain:python_version=3.8 --platforms=//infrastructure/build/platforms:macos_x86_64'
+alias ray_run="aws-vault exec tecton-dev-coral -- python $TECTON_REPO_PATH/tools/personal/brian/anyscale_tecton_job/run_anyscale.py"
 
 #-------------------------------------------------------------------------------
 #-----------------------------------tectonctl-----------------------------------
