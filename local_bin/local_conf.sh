120639cfaadebc2d8db978c0b38245f85d00f4fc
Brian Hart <brian@tecton.ai>
2024-08-30 10:36:48 -0700

    Update local_bin/local_conf.sh

--- local_bin/local_conf.sh
+++ local_bin/local_conf.sh
@@ -20,15 +20,7 @@ alias start-grpc-gateway='(cdt && bazel run //go/cmd/grpc_gateway:grpc_gateway -
 alias repl="(cdt && bazel run //sdk:repl)"
 alias devton='bazel run //sdk:declarative_cli --'
 alias duckdb_s3="$VENV_DIR/data/bin/python -i ~/bin/duckdb_s3.py"
-alias ray_run="aws-vault exec tecton-dev-coral -- $VENV_DIR/anyscale/bin/python $TECTON_REPO_PATH/tools/embeddings_ray_job/run_anyscale.py"
 alias pre-commit="uv tool run pre-commit"
-
-function ray_stats() {
-	_BUCKET="s3://tecton-dev-coral-misc-data"
-	_OBJ=$(aws-vault exec tecton-dev-coral -- aws s3 ls --recursive "$_BUCKET/bench-output/" | awk '{ print $4 }' | fzf --height 50% --border)
-	_FILE="$_BUCKET/$_OBJ"
-	aws-vault exec tecton-dev-coral -- "$VENV_DIR/data/bin/python" -i "$TECTON_REPO_PATH/tools/embeddings_ray_job/analyze_stats.py" "$_FILE"
-}
 
 #-------------------------------------------------------------------------------
 #-----------------------------------tectonctl-----------------------------------
