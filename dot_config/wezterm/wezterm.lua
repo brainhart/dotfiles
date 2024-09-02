bf3742299ae870fa9469133f1334b3a1a5e76729
Brian Hart <brian@tecton.ai>
2024-09-02 11:10:38 -0700

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -8,6 +8,7 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.color_scheme = "Gruvbox dark, pale (base16)"
+config.hide_tab_bar_if_only_one_tab = true
 
 -- and finally, return the configuration to wezterm
 return config
