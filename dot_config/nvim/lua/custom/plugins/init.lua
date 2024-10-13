d19e2756c92575362bdcc068769c4634332437b7
Brian Hart <brian@tecton.ai>
2024-10-12 19:54:26 -0700

    Add .config/nvim/lua/custom/plugins/init.lua

---
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,50 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+    dependencies = {
+      'nvim-telescope/telescope.nvim', -- Only needed if you want to use session lens
+    },
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/code/scratch', '~/Downloads', '/', '/tmp' },
+      -- log_level = 'debug',
+    },
+  },
+
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    build = ':Copilot auth',
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+
+  {
+    'zbirenbaum/copilot-cmp',
+    dependencies = 'copilot.lua',
+    opts = {},
+    config = function(_, opts)
+      local copilot_cmp = require 'copilot_cmp'
+      copilot_cmp.setup(opts)
+      -- attach cmp source whenever copilot attaches
+      -- fixes lazy-loading issues with the copilot cmp source
+      -- LazyVim.lsp.on_attach(function(client)
+      --  copilot_cmp._on_insert_enter({})
+      -- end, "copilot")
+    end,
+  },
+}
